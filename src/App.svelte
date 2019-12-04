086bfa8caac7568dd41c1f620f374b7d282f717b
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-12-04 15:36:30 +0000

    Firs t commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+ let name ="Mohamed Lafiteh";
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+</main>
+
+<style>
+	
+	h1 {
+		color: #ff3e00;
+	
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
